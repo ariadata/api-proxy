7ff1822aa01ef020daf616ee2dd8d4871603defe
Mehrdad Amini <pcmehrdad@gmail.com>
2025-02-07 21:00:41 +0300

    fix withTimeout ptoblem

--- main.go
+++ main.go
@@ -471,7 +471,7 @@ func main() {
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	<-stop
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Printf("Server shutdown error: %v", err)
